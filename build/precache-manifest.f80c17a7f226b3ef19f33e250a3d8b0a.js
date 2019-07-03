ff700fdbafecf040240d878fb236a7a2830be588
Danil <46662253+DZ666@users.noreply.github.com>
2019-07-03 20:57:41 +0400

    Add files via upload

---
+++ build/precache-manifest.f80c17a7f226b3ef19f33e250a3d8b0a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748d1adf467e1ea8f15c8852cc8e5c1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7f3579f9fbcbc8fa917",
+    "url": "/static/css/main.9e246414.chunk.css"
+  },
+  {
+    "revision": "1bab5ee2d818c872d7f5",
+    "url": "/static/js/2.5608b7b0.chunk.js"
+  },
+  {
+    "revision": "e7f3579f9fbcbc8fa917",
+    "url": "/static/js/main.19769706.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0f1d280603eae7525f28ae8429684d2f",
+    "url": "/static/media/Business.0f1d2806.png"
+  },
+  {
+    "revision": "aa044cc34be88d42fd617cff69642320",
+    "url": "/static/media/Item1.aa044cc3.png"
+  },
+  {
+    "revision": "b8efb9dd2cd34ef5afe4f744ea8d2282",
+    "url": "/static/media/Item2.b8efb9dd.png"
+  },
+  {
+    "revision": "26d9496904d2cfed4077654830051942",
+    "url": "/static/media/Item3.26d94969.png"
+  },
+  {
+    "revision": "d51ca9ece0f14f441c516ebbbada9031",
+    "url": "/static/media/Item4.d51ca9ec.png"
+  },
+  {
+    "revision": "c7cc1f04e0cf9e602930587fe0daec05",
+    "url": "/static/media/OctoTrade.c7cc1f04.png"
+  },
+  {
+    "revision": "fb38a4933b3b1cf3bc46a910787bf594",
+    "url": "/static/media/OctoTrade1.fb38a493.png"
+  },
+  {
+    "revision": "16c2a00023fed1ef8f13955d81487394",
+    "url": "/static/media/bg.16c2a000.jpg"
+  },
+  {
+    "revision": "5599ca78e8e372d34570ff33c5fb41de",
+    "url": "/static/media/pic2.5599ca78.jpg"
+  },
+  {
+    "revision": "81f75be28d4e97a5153e1c7a7a266675",
+    "url": "/static/media/pic3.81f75be2.jpg"
+  },
+  {
+    "revision": "ec6ac9f858d51e70dffb236d73e1d64c",
+    "url": "/static/media/pic4.ec6ac9f8.jpg"
+  }
+]);
